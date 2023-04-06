0c9c3ef56734c9f51bafcbd397fba96fabb47715
lukereative <lexigraphic@gmail.com>
2023-04-06 13:30:02 +1200

    1 file modified, 7 deleted

--- histoire.config.ts
+++ histoire.config.ts
@@ -4,5 +4,4 @@ import { HstNuxt } from '@histoire/plugin-nuxt'
 
 export default defineConfig({
   plugins: [HstVue(), HstNuxt()],
-  setupFile: 'histoire.setup.ts',
 })
--- histoire.setup.ts
+++
@@ -1,6 +0,0 @@
-import './src/index.css'
-
-export function setupVue3({ app }) {
-  app.provide('test', 'hello')
-  // app.use(...)
-}
